0adffd15e8c51e293becccc3dbb93b214697d742
grle0e <chingerel592@gmail.com>
2025-03-28 20:01:40 +0800

    last

--- Java/src/main/java/frogger/Frogger.java
+++ Java/src/main/java/frogger/Frogger.java
@@ -14,7 +14,7 @@ public class Frogger {
     public boolean move(boolean forward) {
         int nextPosition = this.position + (forward ? 1 : -1);
         if (!road.isValidPosition(nextPosition) || road.isPositionOccupied(nextPosition)) {
-            return true;
+            return false;
         }
         this.position = nextPosition;
         return true;
